ac8ecdf27108386462579bb10e61750375650e02
mathusanth2002 <89956973+mathusanth2002@users.noreply.github.com>
2022-08-05 23:35:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,19 +3,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5485848281:AAECnvCl7wOF_SpAzLE3WebAgbJXA8i7Rd4")
 
     SESSION_NAME = os.environ.get("SESSION_NAME", "utubeitbot")
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("API_ID", "5615631")
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "f65d600ff580456871cfddc086337b7a")
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = os.environ.get("CLIENT_ID", "921912615039-42h2eg2tsqcmbig6mae5df8vai54mbff.apps.googleusercontent.com")
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "GnsnVpyT3DT9cJStuexDOJ55")
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1496802577")
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
